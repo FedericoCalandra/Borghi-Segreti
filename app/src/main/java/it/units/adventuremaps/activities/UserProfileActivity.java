9852bcc04ed48b16a09a538e039c2a71fe432696
federico_calandra <federico12597@gmial.com>
2023-02-10 12:33:11 +0100

    Added better UI for UserProfileActivity

--- app/src/main/java/it/units/adventuremaps/activities/UserProfileActivity.java
+++ app/src/main/java/it/units/adventuremaps/activities/UserProfileActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,11 +18,17 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        String s = "Username = " + Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        TextView textView = findViewById(R.id.usernameTextView);
-        textView.setText(s);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String username = "Username:   " + Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName();
+        String email = "Email:   " + Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
+
+        TextView usernameTextView = findViewById(R.id.usernameTextView);
+        usernameTextView.setText(username);
+        TextView emailTextView = findViewById(R.id.emailTextView);
+        emailTextView.setText(email);
     }
 
 }
